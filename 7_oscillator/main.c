ac956d268911d18cfb7670e1d81d9f03ca2768f6
Pelle Garbus <p@garbus.dk>
2017-03-09 19:59:02 +0100

    finished second part of 7_orbit. this was previous named osciallator.

--- 7_oscillator/main.c
+++ 7_oscillator/main.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <gsl/gsl_odeiv2.h>
 #include <gsl/gsl_errno.h>
+#include <math.h>
 
 int differential_function(double t, const double y[], double f[], void *params){
 	(void) t; /*avoid warning about unused variable. 
 	is this parameter needed for stepper function?*/
 	/*we need to implement this ordinary differential equation: y'(x) = y(x)*(1-y(x))*/
 	(void) params;
-	f[0] = y[0]*(1/y[0]); 
+	f[0] = y[0]*(1/y[0]); /*y'(x) = y(x)*(1-y(x))*/
 	return GSL_SUCCESS;
 }
-	/*we don't need to bother about Jacobian matrix - not yet*/
-
+	/*we don't need to bother about Jacobian matrix - not at all*/
+int orbit_function(double phi, const double y[], double f[], void *params){
+	(void) phi;
+	double epsilon = *(double *) params;
+	/*
+	y0' = y1
+	y1' = 1-y0+ε*y0*y0 
+	*/
+	f[0] = y[1];
+	f[1] = 1-y[0]+epsilon*y[0]*y[0];
+	return GSL_SUCCESS;
+}
 int main()
 {
 	{ /*PelleGarbus*/
+		printf("EXERCISE: ORDINARY DIFFERENTIAL SYSTEM\n");
 		size_t dim = 1;
 		void* jacobian = NULL; /*should be pointer*/
 		void* params = NULL; /*should be pointer*/
@@ -42,6 +54,74 @@ int main()
 			printf("x = %8.5g\ty = %8.5g\n", x,*y);
 		}
 		gsl_odeiv2_driver_free(d);
+	}
+
+	{/*PelleGarbus Orbit*/
+		printf("EXERCISE: ORBITING PLANET\n");
+		double input;
+		double params[3];
+		for(int i = 0; i < 3; i++){
+			scanf("%lg",&input);
+			params[i] = input;
+		}
+		size_t dim = 2;
+		void* jacobian = NULL;
+		double* epsilon = &params[0];/*need to be a list or similar with 0, 0, 0.1*/
+		gsl_odeiv2_system orbit_system = {orbit_function, jacobian, dim, &epsilon};
+		double hstart = 1e-1, epsabs = 1e-6, epsrel = 0;
+		gsl_odeiv2_driver* orbit_driver = gsl_odeiv2_driver_alloc_y_new(
+			&orbit_system,
+			gsl_odeiv2_step_rk8pd,
+			hstart,
+			epsabs,
+			epsrel
+			);
+		double modified_t = 0.0; /*ths parameter will be constantly 
+		changed to drive the system from t_initial to t_final*/
+		double u0 = params[1], u1 = params[2]; /*extract information from a matrix and iterate.*/
+		double u[2] = {u0, u1}; /*will contain the values obtained 
+		from the driver_applied inside the for-loop for each phi-step.*/
+		double phi_min = 0, phi_max = 39.5 * M_PI, delta_phi = 0.5; /*blindly copied from Fedorov*/
+		printf("%10s %10s %10s\n", "phi", "y0", "y1");
+		for (double phi = phi_min; phi < phi_max; phi += delta_phi){
+			int status = gsl_odeiv2_driver_apply(orbit_driver, &modified_t,phi,u);
+			if (status != GSL_SUCCESS){
+				fprintf(stderr, "Error, returned status = %d\n", status);
+				break;
+			}
+			// if(fmod(phi,5.0) == 0.0) printf("%10.7g %10.7g %10.7g\n", phi,u[0],u[1]);
+			printf("%10.7g %10.7g %10.7g\n", phi,u[0],u[1]);
+		}
+		gsl_odeiv2_driver_free(orbit_driver);
+		/*
+		*/
+
+		/* Andreas Gas
+	    {
+	    printf("\n\n");
+	    double t=0.0;
+	    double hstart = 1e-3, epsabs =1e-6 , epsrel =1e-6 ;
+	    double params[3];
+	    double x;
+	    for(int i = 0; i<3; i++){
+	        scanf("%lg",&x);
+	        params[i]=x;}
+
+	    int dimension = 2;
+	    double epsilon = params[0], u[2] = {params[1],params[2]};
+	    double phi_min = 0, phi_max = 39.5/2 * M_PI, delta_phi = 0.05;
+	    gsl_odeiv2_system orbit = {orbit_func, NULL, dimension, &epsilon};
+	    gsl_odeiv2_driver *driver_orbit = gsl_odeiv2_driver_alloc_y_new(
+	        &orbit,gsl_odeiv2_step_rk8pd,hstart,epsabs,epsrel);
+
+	    for(double phi = phi_min; phi<phi_max;phi+=delta_phi){
+	        int status = gsl_odeiv2_driver_apply(driver_orbit,&t,phi,u);
+	        printf("%g %g\n",phi,u[0]);
+			if (status != GSL_SUCCESS) fprintf (stderr, "fun: status=%i", status);
+	    }
+	    gsl_odeiv2_driver_free(driver_orbit);}
+
+		*/
 	}
 	return 0;
 }
